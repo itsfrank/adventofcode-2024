035ce856c508fb7fed53cb91f812ca0d1826e7e4
itsfrank <frankobrien11@yahoo.ca>
2024-12-03 20:01:53 -0800

    fix template tests

--- lib/day_x/day_x.ml
+++ lib/day_x/day_x.ml
@@ -10,27 +10,27 @@ module Test = struct
   open Alcotest
 
   let%test "parse input" =
-    let data = Common.resource_file "day_2/sample.txt" |> parse_input in
+    let data = Common.resource_file "day_x/sample.txt" |> parse_input in
     (check int) "c" 1 data
   ;;
 
   let%test "s1 sample" =
-    let res = Common.resource_file "day_2/sample.txt" |> parse_input |> do_part1 in
+    let res = Common.resource_file "day_x/sample.txt" |> parse_input |> do_part1 in
     (check int) "s1 sample" 1 res
   ;;
 
   let%test "s1 main" =
-    let res = Common.resource_file "day_2/puzzle.txt" |> parse_input |> do_part1 in
+    let res = Common.resource_file "day_x/puzzle.txt" |> parse_input |> do_part1 in
     (check int) "s1 puzzle" 1 res
   ;;
 
   let%test "s2 sample" =
-    let res = Common.resource_file "day_2/sample.txt" |> parse_input |> do_part2 in
+    let res = Common.resource_file "day_x/sample.txt" |> parse_input |> do_part2 in
     (check int) "s2 sample" 1 res
   ;;
 
   let%test "s2 main" =
-    let res = Common.resource_file "day_2/puzzle.txt" |> parse_input |> do_part2 in
+    let res = Common.resource_file "day_x/puzzle.txt" |> parse_input |> do_part2 in
     (check int) "s2 puzzle" 1 res
   ;;
 end
